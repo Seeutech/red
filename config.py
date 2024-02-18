4cee7d7927e13265de3fb66018913476dfe54733
Devil X - Sameer Khan <116139678+devilxsam@users.noreply.github.com>
2024-02-18 17:19:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,9 @@ PRIVATE_CHAT_ID = -1002041228509  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = "csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; __bid_n=18db31bdad1e0ee8094207; __stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
 ADMINS = [6271019610]
 PREMIUM_USERS = [6271019610, 5952619139]
+YOUR_ADMIN_USERNAME = "xTMAdminBot"
+PREMIUM_PLANS = {
+    "𝐏𝐥𝐚𝐧 1": {"amount": 30.0, "validity_days": 15},
+    "𝐏𝐥𝐚𝐧 2": {"amount": 60.0, "validity_days": 30},
+    "𝐏𝐥𝐚𝐧 3": {"amount": 90.0, "validity_days": 45},
+}
