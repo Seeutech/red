47af9a17f4b52f0b9c38d47a13ace6974f935770
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-03-21 17:52:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 API_ID = 23054736  # api id
 API_HASH = "d538c2e1a687d414f5c3dce7bf4a743c"  # api hash
 
-BOT_TOKEN = "6758593032:AAFzvPvHx12Q8k0K3bFcA0rAOkJtGjhtcVU"  # bot token
+BOT_TOKEN = "7054995045:AAErBTKXTY9QG-UmFMLtJXsb-wQAVAHqi0s"  # bot token
 
 
 ## REDIS
